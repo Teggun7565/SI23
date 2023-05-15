3f71e75c0e61070515a9bf4cc9c45ea5ad0e1c6a
JESUS GUTIERREZ <jesus.gutierrez@cetysuniversidad.edu.mx>
2023-05-14 22:15:43 -0700

    network file & training file, falta testeo

--- proyecto_2_DL/network.py
+++ proyecto_2_DL/network.py
@@ -12,9 +12,22 @@ class Network(nn.Module):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
         # TODO: Calcular dimension de salida
-        out_dim = ...
+        out_dim = self.calc_out_dim(input_dim, kernel_size=3, stride=1, padding=0)
+        out_dim = self.calc_out_dim(out_dim, kernel_size=3, stride=1, padding=0)
+        out_dim = self.calc_out_dim(out_dim, kernel_size=3, stride=1, padding=0)
+        out_dim = self.calc_out_dim(out_dim, kernel_size=3, stride=1, padding=0)
 
         # TODO: Define las capas de tu red
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=0)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=0)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.fc1 = nn.Linear(128*out_dim*out_dim, 256)
+        self.fc2 = nn.Linear(256, n_classes)
 
         self.to(self.device)
  
@@ -24,11 +37,27 @@ class Network(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # TODO: Define la propagacion hacia adelante de tu red
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        logits = self.fc2(x)
+        proba = F.softmax(logits, dim=1)
         return logits, proba
 
     def predict(self, x):
         with torch.inference_mode():
             return self.forward(x)
+
+    def mi_inferencia(self, x):
+        with torch.no_grad():
+            self.eval()
+            x = x.to(self.device)
+            _, proba = self.forward(x.unsqueeze(0))
+            pred_class = torch.argmax(proba).item()
+        return pred_class
 
     def save_model(self, model_name: str):
         '''
@@ -39,7 +68,7 @@ class Network(nn.Module):
         '''
         models_path = file_path / 'models' / model_name
         # TODO: Guarda los pesos de tu red neuronal en el path especificado
-        torch.save( ... )
+        torch.save(self.state_dict(), models_path)
 
     def load_model(self, model_name: str):
         '''
@@ -47,4 +76,8 @@ class Network(nn.Module):
             args:
             - path (str): path relativo donde se guardó el modelo
         '''
+        models_path = file_path / 'models' / model_name
+        assert models_path.exists(), f"El archivo {models_path} no existe"
         # TODO: Carga los pesos de tu red neuronal
+        self.load_state_dict(torch.load(models_path))
+        self.to(self.device)
